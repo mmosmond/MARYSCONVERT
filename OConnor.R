6f1a5d377be9df2a20ff6719ab7ceb35d4264889
Michael Scott <mscott0106@dhcp-128-189-69-116.ubcsecure.wireless.ubc.ca>
2016-01-26 15:46:26 -0800

    produce SAD and SAR plots from Patrick's data

--- OConnor.R
+++ OConnor.R
@@ -1,53 +1,18 @@
-####### GIT WORKS!!!#####
-
-#here the dimensions of the ecosystem area are inputted. These numbers refer to the 1000m x 500m of the BCI forest
-Lx<-20
-Ly<-20
-
-#Here the area of the plots is inputted. This number gives the level of resolution of the analysis. You need to choose a resolution so to have enough statistics and at the same time reduces the noise. The output of the PCF gives you an idea of this trade off: you need to have a n>30 points and not too noisy
-Asub<-4
-
-###NOTE THAT Lx/Asub should be an integer, otherwise some plots will extend over the edge of the area or be lost. 
-
-###################################################
-################## Generate data ##################
-set.seed(1234)
-
-#number of samples
-nsamp<-1000
-#number of species
-nspecies=3
-
-#position of each sample will be drawn from a normal distribution with the following parameters (in the middle of the study area)
-meanx<-Lx/2
-meany<-Ly/2
-sdx<-Lx/5
-sdy<-Ly/5
-xcoor<-rnorm(nsamp,mean=meanx,sd=sdx)
-ycoor<-rnorm(nsamp,mean=meany,sd=sdy)
-#Species names are given by sequential letters
-specieslabels<-sample(letters[1:nspecies],nsamp,replace=TRUE)
-#Abundances at each site are drawn from a uniform distribution between 10 and 100
-abundances<-sample(10:100,nsamp, replace=TRUE)
-
-################ Where data should be imported
-
-#df is the data that will be imported, which should be a data frame with columns of xcoordinate, ycoordinate, species labels and then abundances.
-df<-data.frame(xcoor,ycoor,specieslabels,abundances)
-
-
 ############### Import Patrick's data:
 
 load("/Users/mscott0106/Library/Mail Downloads/Simulation abundances.RData")
 df1<-Abundance.df
-
+### Patrick's data is given as abundances in each patch of a 20x20 grid. The R code below (based on the Mathematica package from Azaele, 2015) does not use this type of abundance data - the number of rows for each species in a "plot" (grid cell) is used as the number of counts in that plot. We can replicate each row according to the abundance column to make the data fit the format required for the package. 
 library(splitstackshape)
 df<-expandRows(df1,"Abundance")
 
+############### Plot Dimensions
+#here the dimensions of the ecosystem area are inputted.
 Lx<-20
 Ly<-20
-Asub<-4
-
+#Here the area of the plots is inputted. This number gives the level of resolution of the analysis. You need to choose a resolution so to have enough statistics and at the same time reduces the noise. The output of the PCF gives you an idea of this trade off: you need to have a n>30 points and not too noisy
+Asub<-2
+###NOTE THAT Lx/Asub should be an integer, otherwise some plots will extend over the edge of the area or be lost. 
 
 ################ summary information
 #species names:
@@ -56,7 +21,7 @@ USpecies<-levels(df[,3])
 NSpecies<-length(USpecies)
 
 ################ Plot information 
-### plots are indexed 1,1 1,2 1,3 etc. and will then be converted back to real distances by multiplying by Asub
+### plots are indexed (1,1) (1,2) (1,3) etc. and will then be converted back to real distances by multiplying by Asub
 
 #number of plots that fit into the area, n.b., should be an integer
 numx<-Lx/Asub
@@ -66,7 +31,7 @@ numy<-Ly/Asub
 allX<-rep(1:numx,each=numy)
 allY<-rep(1:numy,numx)
 allpos<-data.frame(allX,allY)
-#the central position of each plot:
+#the central position of each plot (used for calculating distances)
 xyCpos<-allpos-0.5
 #the total number of plots:
 nplot<-nrow(allpos)
@@ -112,51 +77,50 @@ allpair$allcov<-sapply(1:nrow(allpair),function(x)(CovFunc(x,allpair$possub1,all
 
 ###Take the mean covariance for each distance between plots
 empPCF<-tapply(allpair$allcov,allpair$alldist,mean)
-
+#put the PCF values and their distances in a data frame with appropriate columns
 empPCFdf<-data.frame(as.numeric(rvec)*Asub,as.numeric(empPCF))
 rownames(empPCFdf)<-1:nrow(empPCFdf)
 colnames(empPCFdf)<-c("distance","PCF")
 
-plot(empPCFdf$distance,empPCFdf$PCF)
+plot(empPCFdf)
 
+#We will fit a model that is always Infinite when distance=0. So we remove the first row to help fit
 x<-empPCFdf$distance
 y<-empPCFdf$PCF
 dataset<-data.frame(x,y)
 dataset<-dataset[-1,]
 
 #model often doesn't converge well enough. 
-modelfit<-nls(y ~ 1 + (1/(2*pi)) * ((ro/lambda)^2) * besselK(x/lambda,0), data=dataset, start=list(ro=10,lambda=1),control=list(minFactor = 1/1024,warnOnly=FALSE))
-
-summary(modelfit)
-plot(empPCFdf)
-lines(dataset$x,predict(modelfit))
-
-gFunc<-function(x,ro,lambda){
-	1 + (1/(2*pi)) * ((ro/lambda)^2) * besselK(x/lambda,0)
-}
-lines(seq(1,100,by=0.25),sapply(seq(1,100,by=0.25),function(x)(gFunc(x,roEst,lamEst))))
+modelfit<-nls(y ~ 1+ (1/(2*pi)) * ((ro/lambda)^2) * besselK(x/lambda,0), data=dataset, start=list(ro=10,lambda=1),control=list(minFactor = 1/1024,warnOnly=FALSE))
 
 foo<-summary(modelfit)
 params<-c(foo$parameters[1,1],foo$parameters[2,1])
 roEst<-params[1]
 lamEst<-params[2]
-params
 
-ind<-sapply(1:nplot,function(x)(sum(SabA(x,allpos$possub)))) #number of individuals in each plot (note, doesn't sum to 1000 due to some points being drawn from outside the study area)
-totalind<-sum(ind) #total number of points present
+gFunc<-function(x,ro,lambda){
+	1 + (1/(2*pi)) * ((ro/lambda)^2) * besselK(x/lambda,0)
+}
+plot(empPCFdf)
+lines(seq(1,100,by=0.25),sapply(seq(1,100,by=0.25),function(x)(gFunc(x, roEst, lamEst))))
 
-###functions from Mathematica file
+#number of individuals in each plot
+ind<-sapply(1:nplot,function(x)(sum(SabA(x,allpos$possub)))) 
+#total number of individuals
+totalind<-sum(ind)
+
+###functions taken from Mathematica file, used to generate SAR and SAD curved
 alpha2p<-function(L,ro,lambda){
-	pi * ((L/ro)^2)*(1-(2*lambda/L) * (besselK(L/lambda,1) * besselK(L/lambda,1)) / (besselK(L/lambda,1) * besselI(L/lambda,0) + besselK(L/lambda,0)*besselI(L/lambda,1) ))^(-1)
+	pi * ((L/ro)^2)*(1-(2*lambda/L) * (besselK(L/lambda,1) * besselI(L/lambda,1)) / (besselK(L/lambda,1) * besselI(L/lambda,0) + besselK(L/lambda,0) * besselI(L/lambda,1) ))^(-1)
 }
 beta2<-function(L,ro,lambda){
-	(totalind/(NSpecies*Lx*Ly))*ro^2 *(1-(2*lambda/L)*(besselK(L/lambda,1)*besselI(L/lambda,1))/(besselK(L/lambda,1)*besselI(L/lambda,0)+besselK(L/lambda,0)*besselI(L/lambda,1)))
+	(totalind/(NSpecies * Lx * Ly))* ro^2 *(1-(2*lambda/L) * (besselK(L/lambda,1) * besselI(L/lambda,1)) / (besselK(L/lambda,1) * besselI(L/lambda,0) + besselK(L/lambda,0) * besselI(L/lambda,1) ))
 }
 rsapres2medp<-function(n,L,ro,lambda){
 	pgamma(2^(n+1),alpha2p(L,ro,lambda),scale=beta2(L,ro,lambda))-pgamma(2^n,alpha2p(L,ro,lambda),scale=beta2(L,ro,lambda))
 }
 sarmedp<-function(L,ro,lambda){
-	pgamma(1,alpha2p(L,ro,lambda),scale=beta2(L,ro,lambda))
+	1-pgamma(1,alpha2p(L,ro,lambda),scale=beta2(L,ro,lambda))
 }
 
 #SAR used for downscaled SAR at radius r when all the information in the study region is available. The largest scale is Lx*Ly. Here, r<sqrt(Lx*Ly/pi)
@@ -164,10 +128,12 @@ SAR<-function(r,ro,lambda){
 	NSpecies*(sarmedp(r,ro,lambda))/(sarmedp(sqrt((Lx*Ly)/pi),ro,lambda))
 }
 
-plot(SAR(2:(round(sqrt((Lx*Ly)/pi))),roEst,lamEst))
-plot(SAR(2:10,roEst,lamEst))
+plot(SAR(1:14,roEst,lamEst),xlab="Radius (m) of sampled area",ylab="Mean number of species")
 
 
+#Downscaled SAD at radius r when all the information in the study region is available. The largest scale is Lx*Ly. Here r<sqrt((Lx*Ly)/pi)
+SAD<-function(n,r,ro,lambda){
+	NSpecies*(rsapres2medp(n,r,ro,lambda))/(sarmedp(sqrt((Lx*Ly)/pi),ro,lambda))
+}
 
-
-
+plot(SAD(1:20, 5,roEst,lamEst),xlab="Preston Classes",ylab="Number of species")
