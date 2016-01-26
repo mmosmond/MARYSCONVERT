178d17ef88bf98d803819e05b2c9936675d92191
Michael Scott <mscott0106@dhcp-128-189-69-116.ubcsecure.wireless.ubc.ca>
2016-01-26 12:41:49 -0800

    fixed cov function

--- OConnor.R
+++ OConnor.R
@@ -1,9 +1,11 @@
+####### GIT WORKS!!!#####
+
 #here the dimensions of the ecosystem area are inputted. These numbers refer to the 1000m x 500m of the BCI forest
-Lx<-1000
-Ly<-500
+Lx<-20
+Ly<-20
 
 #Here the area of the plots is inputted. This number gives the level of resolution of the analysis. You need to choose a resolution so to have enough statistics and at the same time reduces the noise. The output of the PCF gives you an idea of this trade off: you need to have a n>30 points and not too noisy
-Asub<-100
+Asub<-4
 
 ###NOTE THAT Lx/Asub should be an integer, otherwise some plots will extend over the edge of the area or be lost. 
 
@@ -33,13 +35,25 @@ abundances<-sample(10:100,nsamp, replace=TRUE)
 #df is the data that will be imported, which should be a data frame with columns of xcoordinate, ycoordinate, species labels and then abundances.
 df<-data.frame(xcoor,ycoor,specieslabels,abundances)
 
+
+############### Import Patrick's data:
+
+load("/Users/mscott0106/Library/Mail Downloads/Simulation abundances.RData")
+df1<-Abundance.df
+
+library(splitstackshape)
+df<-expandRows(df1,"Abundance")
+
+Lx<-20
+Ly<-20
+Asub<-4
+
+
 ################ summary information
 #species names:
 USpecies<-levels(df[,3])
 #number of different species
 NSpecies<-length(USpecies)
-#total number of individuals in the ecosystem
-TotAbundance=sum(df[,4])
 
 ################ Plot information 
 ### plots are indexed 1,1 1,2 1,3 etc. and will then be converted back to real distances by multiplying by Asub
@@ -61,9 +75,8 @@ nplot<-nrow(allpos)
 #For each species point (row of the original data), what plot is it in?
 positions<-ceiling(df[,1:2]/Asub)
 #XYpos will be used to go through all the plots in allpos and return a list with the row number in the original data if present in that plot 
-XYpos<-function(i){intersect(which(positions$xcoor==allX[i]),which(positions$ycoor==allY[i]))}
+XYpos<-function(i){intersect(which(positions[,1]==allX[i]),which(positions[,2]==allY[i]))}
 allpos$possub<-sapply(1:nplot,function(x)(XYpos(x)))
-
 
 ################ Pairwise comparisons between plots (will also copy across possub)
 #all possible combinations of plot positions
@@ -89,32 +102,72 @@ rvec<-levels(allpair$alldist)
 ################ Calculate covariances between each pair (allcov)
 #SabA gets the counts of the species in each plot using poslist
 SabA<-function(i,poslist){
-	table(df$specieslabels[poslist[[i]]])
+	table(df[poslist[[i]],3])
 	}
 #CovFunc will use these counts to get the covariance between pairs 
 CovFunc<-function(i,poslist1,poslist2){
-	mean(SabA(i,poslist1)*SabA(i,poslist2))-mean(SabA(i,poslist1))*mean(SabA(i,poslist2))
+	mean(SabA(i,poslist1)*SabA(i,poslist2))/(mean(SabA(i,poslist1))*mean(SabA(i,poslist2)))
 }
 allpair$allcov<-sapply(1:nrow(allpair),function(x)(CovFunc(x,allpair$possub1,allpair$possub2)))
 
 ###Take the mean covariance for each distance between plots
 empPCF<-tapply(allpair$allcov,allpair$alldist,mean)
-#convert row names back to real distances.
-rownames(empPCF)<-as.numeric(rvec)*Asub
 
 empPCFdf<-data.frame(as.numeric(rvec)*Asub,as.numeric(empPCF))
 rownames(empPCFdf)<-1:nrow(empPCFdf)
 colnames(empPCFdf)<-c("distance","PCF")
 
-#model fitting
-empPCFdf <- empPCFdf[which(empPCFdf$distance != 0),] #remove rows with distance of zero because this breaks the bessel function
-m <- nls(PCF ~ 1 + 1/(2*pi) * (ro/lambda) * besselK(abs(distance/lambda), 0), data = empPCFdf, start = list(ro = 10000, lambda = 50000), trace = T) #take absolute value because with made up data nls looks for negative lambda, which breaks bessel (set lower bounds somehow?)
-#m <- nls(y ~ 1 + 1/(2*pi) * (ro/lambda) * besselK(x/lambda, 0), data = df, start = list(ro = 10000, lambda = 50000), trace = T, lower = list(0, 0))
+plot(empPCFdf$distance,empPCFdf$PCF)
 
-summary(m) #parameter estimates
-plot(empPCFdf) #data
-lines(empPCFdf$distance ,predict(m)) #fitted curve
+x<-empPCFdf$distance
+y<-empPCFdf$PCF
+dataset<-data.frame(x,y)
+dataset<-dataset[-1,]
 
-install.packages("propagate")
-library(propagate)
-	⁃	predictNLS(m, newdata = data.frame(	distance = 2))
+#model often doesn't converge well enough. 
+modelfit<-nls(y ~ 1 + (1/(2*pi)) * ((ro/lambda)^2) * besselK(x/lambda,0), data=dataset, start=list(ro=10,lambda=1),control=list(minFactor = 1/1024,warnOnly=FALSE))
+
+summary(modelfit)
+plot(empPCFdf)
+lines(dataset$x,predict(modelfit))
+
+gFunc<-function(x,ro,lambda){
+	1 + (1/(2*pi)) * ((ro/lambda)^2) * besselK(x/lambda,0)
+}
+lines(seq(1,100,by=0.25),sapply(seq(1,100,by=0.25),function(x)(gFunc(x,roEst,lamEst))))
+
+foo<-summary(modelfit)
+params<-c(foo$parameters[1,1],foo$parameters[2,1])
+roEst<-params[1]
+lamEst<-params[2]
+params
+
+ind<-sapply(1:nplot,function(x)(sum(SabA(x,allpos$possub)))) #number of individuals in each plot (note, doesn't sum to 1000 due to some points being drawn from outside the study area)
+totalind<-sum(ind) #total number of points present
+
+###functions from Mathematica file
+alpha2p<-function(L,ro,lambda){
+	pi * ((L/ro)^2)*(1-(2*lambda/L) * (besselK(L/lambda,1) * besselK(L/lambda,1)) / (besselK(L/lambda,1) * besselI(L/lambda,0) + besselK(L/lambda,0)*besselI(L/lambda,1) ))^(-1)
+}
+beta2<-function(L,ro,lambda){
+	(totalind/(NSpecies*Lx*Ly))*ro^2 *(1-(2*lambda/L)*(besselK(L/lambda,1)*besselI(L/lambda,1))/(besselK(L/lambda,1)*besselI(L/lambda,0)+besselK(L/lambda,0)*besselI(L/lambda,1)))
+}
+rsapres2medp<-function(n,L,ro,lambda){
+	pgamma(2^(n+1),alpha2p(L,ro,lambda),scale=beta2(L,ro,lambda))-pgamma(2^n,alpha2p(L,ro,lambda),scale=beta2(L,ro,lambda))
+}
+sarmedp<-function(L,ro,lambda){
+	pgamma(1,alpha2p(L,ro,lambda),scale=beta2(L,ro,lambda))
+}
+
+#SAR used for downscaled SAR at radius r when all the information in the study region is available. The largest scale is Lx*Ly. Here, r<sqrt(Lx*Ly/pi)
+SAR<-function(r,ro,lambda){
+	NSpecies*(sarmedp(r,ro,lambda))/(sarmedp(sqrt((Lx*Ly)/pi),ro,lambda))
+}
+
+plot(SAR(2:(round(sqrt((Lx*Ly)/pi))),roEst,lamEst))
+plot(SAR(2:10,roEst,lamEst))
+
+
+
+
+
