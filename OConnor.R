09cd4268cdd064c270c04424ba1514213ea105ec
mmosmond <mmosmond@gmail.com>
2016-01-30 12:26:27 -0800

    commented R code and saved reef plots

--- OConnor.R
+++ OConnor.R
@@ -1,54 +1,97 @@
+#had to put this function in because splitstackshape package didnt seem to have it 
+expandRows <- function(dataset, count, count.is.col = TRUE, drop = TRUE) {
+
+  if (isTRUE(count.is.col)) {
+
+    vals <- dataset[[count]]
+
+    out <- dataset[rep(sequence(nrow(dataset)), vals), ]
+
+    if (isTRUE(drop)) out[[count]] <- NULL
+
+  } else if (length(count) == 1) {
+
+    vals <- count
+
+    out <- dataset[rep(sequence(nrow(dataset)), each = vals), ]
+
+  } else {
+
+    vals <- count
+
+    out <- dataset[rep(sequence(nrow(dataset)), vals), ]
+
+  }
+
+  if (any(vals == 0)) {
+
+    mess <- sprintf("The following rows have been dropped from the input: \n\n%s\n", 
+
+                    paste(which(vals == 0), collapse = ", "))
+
+    message(mess)
+
+  }
+
+  out
+
+}
+
 ############### Import Patrick's data:
 
-load("/Users/mscott0106/Library/Mail Downloads/Simulation abundances.RData")
+# load("/Users/mscott0106/Library/Mail Downloads/Simulation abundances.RData")
+load("SimulationAbundances.RData")
 df1<-Abundance.df
 ### Patrick's data is given as abundances in each patch of a 20x20 grid. The R code below (based on the Mathematica package from Azaele, 2015) does not use this type of abundance data - the number of rows for each species in a "plot" (grid cell) is used as the number of counts in that plot. We can replicate each row according to the abundance column to make the data fit the format required for the package. 
-library(splitstackshape)
+# library(splitstackshape)
 df<-expandRows(df1,"Abundance")
 # some rows are removed because the species is absent. These species are removed as factors. 
 df$Species<-droplevels(df$Species)
 ###Note, Patrick's data gives the y coordinate in the first column and x coordinate in the second column. The grid is 20*20 so this doesn't matter in this case. 
 
-
 ############### Import Robin's data:
-df1<-read.csv("~/Downloads/sbc_fish_2010.csv")
-head(df1)
-max(df1$Latitude)
-min(df1$Latitude)
-max(df1$Longitude)
-min(df1$Longitude)
 
-library(splitstackshape)
-df2<-expandRows(df1,"Abundance")
+# df1<-read.csv("~/Downloads/sbc_fish_2010.csv")
+df1<-read.csv("sbc_fish_2010.csv")
+# head(df1)
+# # max(df1$Latitude)
+# min(df1$Latitude)
+# max(df1$Longitude)
+# min(df1$Longitude)
 
-df3<-df2[,6:7]
-df3$SpeciesName<-as.factor(with(df2,paste(Genus,Species)))
-nrow(df3)
-df3$Latitude<-(df3$Latitude-min(df1$Latitude))*10+0.01
-df3$Longitude<-(df3$Longitude-min(df1$Longitude))*10+0.01
+# library(splitstackshape)
+df2<-expandRows(df1,"Abundance") #make row for each instance a species was sampled (instead of an abundance column we can just sum over rows)
+
+df3<-df2[,6:7] #lat and long
+df3$SpeciesName<-as.factor(with(df2,paste(Genus,Species))) #latin binomial
+# nrow(df3)
+df3$Latitude<-(df3$Latitude-min(df1$Latitude)) #scale lat so that smallest lat is zero
+df3$Longitude<-(df3$Longitude-min(df1$Longitude)) #scale long so that smallest long is zero
 df<-df3
 
-plot(df[,1:2])
-
+plot(df[,1:2]) #locations of data
 
 ############### Plot Dimensions
+
 #here the dimensions of the ecosystem area are inputted.
-Lx<-4.5
-Ly<-8
+Lx<-max(df$Latitude) #total lat distance
+Ly<-max(df$Longitude) #total long distance
 #Here the area of the plots is inputted. This number gives the level of resolution of the analysis. You need to choose a resolution so to have enough statistics and at the same time reduces the noise. The output of the PCF gives you an idea of this trade off: you need to have a n>30 points and not too noisy
-Asub<-0.5
+Asub<-min(Lx/4, Ly/4) #size of edge of bins (the denominator gives the number of bins you will have along the shortest dimension of total area)
 ###NOTE THAT Lx/Asub should be an integer, otherwise some plots will extend over the edge of the area or be lost. 
 
 ################ summary information
+
 #species names:
 USpecies<-levels(df[,3])
 #number of different species
 NSpecies<-length(USpecies)
 
 ################ Plot information 
+
 ### plots are indexed (1,1) (1,2) (1,3) etc. and will then be converted back to real distances by multiplying by Asub
 
-#number of plots that fit into the area, n.b., should be an integer
+#number of plots that fit into the area in each dimension
 numx<-Lx/Asub
 numy<-Ly/Asub
 
@@ -62,6 +105,7 @@ xyCpos<-allpos-0.5
 nplot<-nrow(allpos)
 
 ################ Which species are in each plot (possub)
+
 #For each species point (row of the original data), what plot is it in?
 positions<-ceiling(df[,1:2]/Asub)
 #XYpos will be used to go through all the plots in allpos and return a list with the row number in the original data if present in that plot 
@@ -69,6 +113,7 @@ XYpos<-function(i){intersect(which(positions[,1]==allX[i]),which(positions[,2]==
 allpos$possub<-sapply(1:nplot,function(x)(XYpos(x)))
 
 ################ Pairwise comparisons between plots (will also copy across possub)
+
 #all possible combinations of plot positions
 allpair<-data.frame(rep(allX,each=nplot),rep(allY,each=nplot),rep(allX,nplot),rep(allY,nplot))
 colnames(allpair)<-c("Xpair1","Ypair1","Xpair2","Ypair2")
@@ -78,6 +123,7 @@ allpair$possub2<-rep(rep(allpos$possub,nplot))
 
 
 ################ Calculate distances between each plot (alldist)
+
 # Rather than using the indexes to each plot, the central position of each plot will be used to calculate the distances between particular pairs of plots (note that, this is not necessary, distances are just measured relative to one another so you could use the indexes)
 allpairCpos<-cbind(rep(allX,each=nplot),rep(allY,each=nplot),rep(allX,nplot),rep(allY,nplot))-0.5
 #dist takes the euclidian distance using pythagoras' rule
@@ -90,6 +136,7 @@ allpair$alldist<-as.factor(sapply(1:npair,function(x)(dist(x))))
 rvec<-levels(allpair$alldist)
 
 ################ Calculate covariances between each pair (allcov)
+
 #SabA gets the counts of the species in each plot using poslist
 SabA<-function(i,poslist){
 	table(df[poslist[[i]],3])
@@ -104,7 +151,8 @@ sapply(1:50,function(x)(CovFunc(x,allpair$possub1,allpair$possub2)))
 ### because there are some grid cells with no data, I simply omit these rows from allpair. 
 allpair<-na.omit(allpair)
 
-###Take the mean covariance for each distance between plots
+################ Take the mean covariance for each distance between plots
+
 allpair$alldist<-droplevels(allpair$alldist)
 rvec<-levels(allpair$alldist)
 empPCF<-tapply(allpair$allcov,allpair$alldist,mean)
@@ -114,6 +162,8 @@ rownames(empPCFdf)<-1:nrow(empPCFdf)
 colnames(empPCFdf)<-c("distance","PCF")
 
 plot(empPCFdf)
+
+################ Model fit and plot result
 
 #We will fit a model that is always Infinite when distance=0. So we remove the first row to help fit
 x<-empPCFdf$distance
@@ -132,10 +182,13 @@ lamEst<-params[2]
 gFunc<-function(x,ro,lambda){
 	 1+(1/(2*pi)) * ((ro/lambda)^2) * besselK(x/lambda,0)
 }
+
+postscript('ReefempPCF.eps',width=8)
 plot(empPCFdf)
 lines(seq(0,20,by=0.01),sapply(seq(0,20,by=0.01),function(x)(gFunc(x, roEst, lamEst))))
+dev.off()
 
-empPCFdf
+################ functinos for SAR and SAD curves
 
 #number of individuals in each plot
 ind<-sapply(1:nplot,function(x)(sum(SabA(x,allpos$possub)))) 
@@ -156,12 +209,25 @@ sarmedp<-function(L,ro,lambda){
 	1-pgamma(1,alpha2p(L,ro,lambda),scale=beta2(L,ro,lambda))
 }
 
+########### downscaled SAD and SAR
+
 #SAR used for downscaled SAR at radius r when all the information in the study region is available. The largest scale is Lx*Ly. Here, r<sqrt(Lx*Ly/pi)
 SAR<-function(r,ro,lambda){
 	NSpecies*(sarmedp(r,ro,lambda))/(sarmedp(sqrt((Lx*Ly)/pi),ro,lambda))
 }
 
-plot(SAR(1:14,roEst,lamEst),xlab="Radius (0.1 lat-long coordinates, not corrected) of sampled area",ylab="Mean number of species")
+postscript('ReefdsSAR.eps',width=8)
+dsSAR<-plot(x,SAR(x,roEst,lamEst),xlab="Radius (Euclidean distance between lat-long) of sampled area",ylab="Mean number of species", ylim=c(0,50))
+dev.off()
+
+#Downscaled SAD at radius r when all the information in the study region is available. The largest scale is Lx*Ly. Here r<sqrt((Lx*Ly)/pi)
+SAD<-function(n,r,ro,lambda){
+	NSpecies*(rsapres2medp(n,r,ro,lambda))/(sarmedp(sqrt((Lx*Ly)/pi),ro,lambda))
+}
+
+plot(SAD(1:20, 5,roEst,lamEst),xlab="Preston Classes",ylab="Number of species")
+
+##############  upscaled SAD and SAR
 
 upSAR<-function(r,ro,lambda){
 	NSpecies*(sarmedp(r,ro,lambda))/(sarmedp(sqrt((20*0.1)/pi),ro,lambda))
@@ -170,9 +236,3 @@ upSAR<-function(r,ro,lambda){
 plot(upSAR(1:14,roEst,lamEst),xlab="Radius (0.1 lat-long coordinates, not corrected) of sampled area",ylab="Mean number of species")
 
 
-#Downscaled SAD at radius r when all the information in the study region is available. The largest scale is Lx*Ly. Here r<sqrt((Lx*Ly)/pi)
-SAD<-function(n,r,ro,lambda){
-	NSpecies*(rsapres2medp(n,r,ro,lambda))/(sarmedp(sqrt((Lx*Ly)/pi),ro,lambda))
-}
-
-plot(SAD(1:20, 5,roEst,lamEst),xlab="Preston Classes",ylab="Number of species")
